18e71f348f2f02a965cbf8ffed56f579d763869d
luiz Spinelli <luiz.lucchi@gmail.com>
2020-07-17 16:22:08 -0300

    signIn page

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,23 @@
+import Styled from 'styled-components/native';
+import { RectButton } from 'react-native-gesture-handler';
+
+export const Container = Styled(RectButton)`
+
+  width: 100%;
+  height: 60px;
+  background: #ff9000;
+  border-radius: 10px;
+  margin-top: 8px;
+
+  justify-content: center;
+  align-items: center;
+
+`;
+
+export const ButtonText = Styled.Text`
+
+  font-family: 'RobotoSlab-Medium';
+  color: #312e38;
+  font-size: 18px;
+
+`;
--- src/pages/SignIn/styles.ts
+++ src/pages/SignIn/styles.ts
@@ -1,3 +1,18 @@
 import Styled from 'styled-components/native';
 
-export const Container = Styled.View``;
+export const Container = Styled.View`
+
+  flex: 1;
+  align-items: center;
+  justify-content: center;
+  padding: 0 30px;
+
+`;
+export const Title = Styled.Text`
+
+  font-size: 24px;
+  color: #e4ede8;
+  font-family: 'RobotoSlab-Medium';
+  margin: 64px 0 24px;
+
+`;
